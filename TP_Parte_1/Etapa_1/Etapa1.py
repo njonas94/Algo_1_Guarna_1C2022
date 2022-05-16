12a0cc05a9b91b256a9657fa2239990a15534395
carlicastro <105254519+carlicastro@users.noreply.github.com>
2022-05-16 16:21:32 -0300

    Update Etapa1.py

--- TP_Parte_1/Etapa_1/Etapa1.py
+++ TP_Parte_1/Etapa_1/Etapa1.py
@@ -1,12 +1,4 @@
-def obtener_color(color):
-    colores = {
-        "Verde": "\x1b[32m",
-        "Amarillo": "\x1b[33m",
-        "GrisOscuro": "\x1b[90m",
-        "Defecto": "\x1b[39m"
-    }
-    return colores[color]
-
+from utiles import obtener_color
 def fiuble():
     pal_adiv="MARES"
     print("Palabra a adivinar: ? ? ? ? ?")

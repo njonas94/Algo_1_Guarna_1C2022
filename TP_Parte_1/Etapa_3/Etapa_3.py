540e92f5aa3c88e3f410737b95367819b8fa7990
Nicolás Jonas <91109927+njonas94@users.noreply.github.com>
2022-05-20 19:50:40 -0300

    Update var para la vista del juego

--- TP_Parte_1/Etapa_3/Etapa_3.py
+++ TP_Parte_1/Etapa_3/Etapa_3.py
@@ -26,7 +26,7 @@ def mostrar_juego(lista_palabra_ingresada):
 
 def main():
     palabra_a_adivinar = generar_palabra_a_adivinar()
-    intentos_ingresados = ["? ? ? ? ?", "? ? ? ? ?", "? ? ? ? ?", "? ? ? ? ?", "? ? ? ? ?"]
+    intentos_ingresados = [["?", "?", "?", "?", "?"], ["?", "?", "?", "?", "?"], ["?", "?", "?", "?", "?"], ["?", "?", "?", "?", "?"], ["?", "?", "?", "?", "?"]]
     while True:
         mostrar_juego()
         break
@@ -73,7 +73,7 @@ def procesar_intento(pal_adiv, intento):
 #print("Arriesgo:",colores[0] + intento[0].upper(),colores[1] + intento[1].upper(), colores[2] + intento[2].upper(), colores[3] + intento[3].upper(), colores[4] + intento[4].upper(), obtener_color ("Defecto"))
 
 
-#mostrar_juego(lista_palabra_a_adivinar)
+mostrar_juego(lista_palabra_a_adivinar)
 """
 Pruebas
 

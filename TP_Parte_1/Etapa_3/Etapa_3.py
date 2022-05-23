ef622f90059cdac204a7bb2e8a9cf4962e3521cb
Nicolás Jonas <91109927+njonas94@users.noreply.github.com>
2022-05-23 01:18:52 -0300

    Se trabajo en las funciones para que se acoplen
    Con intencion de llegar a un programa de Etapa 3, se fueron modificando las funciones d elas etapas anteriores, asi como creando nuevas para acercarnos a lo solicitado en la Etapa.

--- TP_Parte_1/Etapa_3/Etapa_3.py
+++ TP_Parte_1/Etapa_3/Etapa_3.py
@@ -1,37 +1,48 @@
 import random
 from utiles import *
 
-palabra_ingresada = ""
-lista_palabra_a_adivinar = ["?", "?", "?", "?", "?"]
-
 def generar_palabra_a_adivinar():
     palabras_validas = obtener_palabras_validas()
     
     return random.choice(palabras_validas)
 
-palabra_a_adivinar = generar_palabra_a_adivinar()
+def acumular_intentos(palabra_ingresada,contador_intentos, intentos_ingresados):
+    for i in range(len(palabra_ingresada)):
+        intentos_ingresados[contador_intentos][i] = palabra_ingresada[i]
 
-def acumular_intentos(palabra_ingresada, intentos_ingresados):
-    if palabra_ingresada not in intentos_ingresados:
-        intentos_ingresados.append(palabra_ingresada)
-    
     return
 
-def mostrar_juego(lista_palabra_ingresada):
-    print("Palabra a adivinar: ", end="")
+def mostrar_palabra(lista_palabra_ingresada):
     for letra in lista_palabra_ingresada:
         print(letra + " ", end= "")
+    print()
 
     return
 
-def main():
-    palabra_a_adivinar = generar_palabra_a_adivinar()
-    intentos_ingresados = [["?", "?", "?", "?", "?"], ["?", "?", "?", "?", "?"], ["?", "?", "?", "?", "?"], ["?", "?", "?", "?", "?"], ["?", "?", "?", "?", "?"]]
-    while True:
-        mostrar_juego()
-        break
+def cambiar_tilde(palabra_ingresada):
+    palabra_mayus = palabra_ingresada.upper()
+    a = 'ÁÉÍÓÚÝÄËÏÖÜŸ'
+    b = 'AEIOUYAEIOUY'
+    palabra_sin_acento = palabra_mayus.maketrans(a, b)
+    arriesgo = palabra_mayus.translate(palabra_sin_acento)
+    
+    return arriesgo
 
-    return
+
+def validacion(palabra_ingresada, intentos_ingresados):
+    verificacion = False
+    if not palabra_ingresada.isalpha():
+        print("La palabra no tiene que contener numero ni caracteres especiales.")
+    if len(palabra_ingresada) != 5:
+        print("La palabra tiene que ser de 5 letras.")
+    if palabra_ingresada not in obtener_palabras_validas():
+        print("La palabra no se encuentra en la lista de palabras válidas.")
+    if palabra_ingresada in intentos_ingresados:
+        print("La palabra ya habia sido ingresada.")
+    if palabra_ingresada.isalpha() and len(palabra_ingresada) == 5 and palabra_ingresada in obtener_palabras_validas() and palabra_ingresada not in intentos_ingresados:
+        verificacion = True
+    
+    return verificacion
 
 def procesar_intento(pal_adiv, intento):
     colores=[]
@@ -70,16 +81,40 @@ def procesar_intento(pal_adiv, intento):
 
     return colores
 
-#print("Arriesgo:",colores[0] + intento[0].upper(),colores[1] + intento[1].upper(), colores[2] + intento[2].upper(), colores[3] + intento[3].upper(), colores[4] + intento[4].upper(), obtener_color ("Defecto"))
+def main():
+    palabra_a_adivinar = generar_palabra_a_adivinar()
+    intentos_ingresados = [["?", "?", "?", "?", "?"], ["?", "?", "?", "?", "?"], ["?", "?", "?", "?", "?"], ["?", "?", "?", "?", "?"], ["?", "?", "?", "?", "?"]]
+    lista_palabra_a_adivinar = ["?", "?", "?", "?", "?"]
+    contador_intentos = 0
+    while contador_intentos < 5:
+        print("Palabra a adivinar: ", end="")
+        mostrar_palabra(lista_palabra_a_adivinar)
+        for intento in intentos_ingresados:
+            mostrar_palabra(intento)
+        arriesgo = input("Arriesgo: ")
+        if validacion(arriesgo, intentos_ingresados):
+            arriesgo = cambiar_tilde(arriesgo)
+            acumular_intentos(arriesgo, contador_intentos, intentos_ingresados)
+            contador_intentos += 1
+            if palabra_a_adivinar == arriesgo:
+                print('Ganaste!')
+            elif contador_intentos == 5: 
+                print('Perdiste!')
+
+    return
 
 
-mostrar_juego(lista_palabra_a_adivinar)
+
+main()
+
 """
 Pruebas
 
 print(intentos_ingresados)
 print(intentos_ingresados[0])
 print(intentos_ingresados[1])
-print("prueba")
+
+print("Arriesgo:",colores[0] + intento[0].upper(),colores[1] + intento[1].upper(), colores[2] + intento[2].upper(), colores[3] + intento[3].upper(), colores[4] + intento[4].upper(), obtener_color ("Defecto"))
+
 
 """

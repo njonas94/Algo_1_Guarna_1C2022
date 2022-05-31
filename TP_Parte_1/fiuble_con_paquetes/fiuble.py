64ce50d3e7814f427e30e223a5ca3e5d0737c05c
carlicastro <105254519+carlicastro@users.noreply.github.com>
2022-05-31 06:02:34 -0300

    Update fiuble.py
    Cambie el nombre de las variables y agregue en la línea 40-41 la condición para que se genere el nuevo orden de turnos si el usuario decide jugar otra partida.
    La función volver_a_jugar en la línea 39, corregida para que devuelva un sólo parámetro

--- TP_Parte_1/fiuble_con_paquetes/fiuble.py
+++ TP_Parte_1/fiuble_con_paquetes/fiuble.py
@@ -16,27 +16,29 @@ def fiuble():
     usuarios_y_puntaje = ingreso_usuarios()#Diccionario: claves: nombres de usuario y valor: puntos acumulados
     turnos = orden_turnos(usuarios_y_puntaje)
     while jugar in ('s','S', ''):
-        lista_de_intentos_ingresados = []
-        pal_adiv = generar_palabra_a_adivinar()
-        adivinar = crear_lista_interrogantes(LONGITUD_PALABRAS)
-        palabras = crear_lista_intentos(CANTIDAD_INTENTOS, adivinar)
-        print("Palabra a adivinar: ", end = "")
-        mostrar_palabra(adivinar)
-        for intento in palabras:
+        lista_de_intentos_ingresados = [] #Lista de intentos, tipo string
+        palabra_a_adivinar = generar_palabra_a_adivinar()
+        lista_letras_palabra_adivinar = crear_lista_interrogantes(LONGITUD_PALABRAS)
+        lista_letras_de_cada_intento = crear_lista_intentos(CANTIDAD_INTENTOS, lista_letras_palabra_adivinar) #Lista de listas, contiene los intentos ingresados,cada letra es un elemento.
+        print("\nPalabra a adivinar: ", end = "")
+        mostrar_palabra(lista_letras_palabra_adivinar)
+        for intento in lista_letras_de_cada_intento:
             mostrar_palabra(intento)
         print("Es el turno de:",turnos[0].upper())
-        inicio = time.time()
-        intento = input("Arriesgo:")
-        intento = validacion_intento_ingresado(intento, lista_de_intentos_ingresados)
-        lista_de_intentos_ingresados = desarrollo_intentos(pal_adiv, intento, turnos, adivinar, palabras) #Esta lista, es la lista de str de palabras ingresadas#
-        fin = time.time()
-        tiempo = cronometro(inicio, fin)
-        if pal_adiv in lista_de_intentos_ingresados:
+        inicio_cronometro = time.time()
+        arriesgo = input("Arriesgo:")
+        arriesgo = validacion_intento_ingresado(arriesgo, lista_de_intentos_ingresados)
+        lista_de_intentos_ingresados = desarrollo_intentos(palabra_a_adivinar, arriesgo, turnos, lista_letras_palabra_adivinar, lista_letras_de_cada_intento) #Esta lista, es la lista de str de palabras ingresadas#
+        fin_cronometro = time.time()
+        tiempo = cronometro(inicio_cronometro, fin_cronometro)
+        if palabra_a_adivinar in lista_de_intentos_ingresados:
             print('Ganaste! Tardaste',tiempo[0],'minutos y',tiempo[1],'segundos')
         else:
-            print(f'Palabra a adivinar: {pal_adiv[0]} {pal_adiv[1]} {pal_adiv[2]} {pal_adiv[3]} {pal_adiv[4]} {obtener_color("Defecto")}\nPerdiste!')
-        puntaje(lista_de_intentos_ingresados,pal_adiv,usuarios_y_puntaje,turnos)
-        jugar, turnos = volver_a_jugar(turnos)
+            print(f'Palabra a adivinar: {palabra_a_adivinar[0]} {palabra_a_adivinar[1]} {palabra_a_adivinar[2]} {palabra_a_adivinar[3]} {palabra_a_adivinar[4]} {obtener_color("Defecto")}\nPerdiste!')
+        puntaje(lista_de_intentos_ingresados,palabra_a_adivinar,usuarios_y_puntaje,turnos)
+        jugar = volver_a_jugar()
+        if jugar in ('s','S'):
+            turnos = nueva_partida(turnos)
     determinar_ganador(usuarios_y_puntaje)
 fiuble()
 

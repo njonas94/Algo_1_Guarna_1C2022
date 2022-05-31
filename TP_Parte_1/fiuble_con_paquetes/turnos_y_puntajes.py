a5936ca46e1507e2b96a70f136a46856b0f7d48b
carlicastro <105254519+carlicastro@users.noreply.github.com>
2022-05-31 07:47:40 -0300

    Update turnos_y_puntajes.py
    Se cambiaron nombres de las variables

--- TP_Parte_1/fiuble_con_paquetes/turnos_y_puntajes.py
+++ TP_Parte_1/fiuble_con_paquetes/turnos_y_puntajes.py
@@ -1,38 +1,39 @@
 import random
 
-def puntaje(lista,adivinar,usuarios,turnos):
+def puntaje(lista_intentos, palabra_adivinar, jugadores_y_puntos, orden_turnos):
     '''
     Función: puntaje
     Parámetros:
-        lista: lista de cadenas de caracteres.
-        adivinar: cadena de caracteres, palabra a adivinar.
-        todos_turnos: lista vacia.
+        lista_intentos: lista cargada con cadenas de caracteres.
+        palabra_adivinar: cadena de caracteres.
+        jugadores_y_puntos: diccionario cargado.
+        orden_turnos: lista cargada.
     Salidas:
         Devuelve la lista cargada con el puntaje obtenido una vez finalizada la partida.
     '''
     valores=[50,40,30,20,10,-50,-100]
-    if adivinar not in lista and len(lista)==5:
-        usuarios[turnos[0]]+= valores[-1]
-        usuarios[turnos[1]]+= valores[-2]
-        print(f"{turnos[0].upper()}, ha perdido, 100 puntos. Tiene acumulados {usuarios[turnos[0]]} puntos.")
-        print(f"{turnos[1].upper()}, ha perdido, 50 puntos. Tiene acumulados {usuarios[turnos[1]]} puntos.")
-    elif adivinar in lista:
-        if len(lista)==1:
+    if palabra_adivinar not in lista_intentos and len(lista_intentos)==5:
+        jugadores_y_puntos[orden_turnos[0]]+= valores[-1]
+        jugadores_y_puntos[orden_turnos[1]]+= valores[-2]
+        print(f"{orden_turnos[0].upper()}, ha perdido, 100 puntos. Tiene acumulados {jugadores_y_puntos[orden_turnos[0]]} puntos.")
+        print(f"{orden_turnos[1].upper()}, ha perdido, 50 puntos. Tiene acumulados {jugadores_y_puntos[orden_turnos[1]]} puntos.")
+    elif palabra_adivinar in lista_intentos:
+        if len(lista_intentos)==1:
             puntos_obtenidos=valores[0]
-        elif len(lista)==2:
+        elif len(lista_intentos)==2:
             puntos_obtenidos=valores[1]
-        elif len(lista)==3:
+        elif len(lista_intentos)==3:
             puntos_obtenidos=valores[2]
-        elif len(lista)==4:
+        elif len(lista_intentos)==4:
             puntos_obtenidos=valores[3]
-        elif len(lista)==5:
+        elif len(lista_intentos)==5:
             puntos_obtenidos=valores[4]
-        indice = len(lista)-1
-        usuarios = contador_puntajes (puntos_obtenidos,usuarios,turnos,indice)
+        indice = len(lista_intentos)-1
+        jugadores_y_puntos = contador_puntajes (puntos_obtenidos, jugadores_y_puntos, orden_turnos, indice)
     
-    return usuarios
+    return jugadores_y_puntos
 
-def contador_puntajes (puntos_obtenidos,usuarios,turnos,indice):
+def contador_puntajes (puntos_obtenidos, usuarios, turnos, indice):
     ''' 
     Funcion que acumula el puntaje para cada jugador.
     '''
@@ -54,13 +55,13 @@ def orden_turnos (usuarios):
     Generador de turnos 
     '''
     turnos = []
-    nombre = list(usuarios.keys())
-    master=random.choice(nombre)
-    indice = nombre.index(master)
+    jugadores = list(usuarios.keys())
+    master=random.choice(jugadores)
+    indice = jugadores.index(master)
     for i in range(5):
         if i in (0,2,4):
             turnos.append(master)              
         else:
-            turnos.append(nombre[indice-1])
+            turnos.append(jugadores[indice-1])
     
     return turnos

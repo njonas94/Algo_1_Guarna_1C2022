de152c222ef36fad212f416af08aae9b9cae6ea4
carlicastro <105254519+carlicastro@users.noreply.github.com>
2022-05-31 09:35:18 -0300

    Update validacion.py

--- TP_Parte_1/fiuble_con_paquetes/validacion.py
+++ TP_Parte_1/fiuble_con_paquetes/validacion.py
@@ -1,22 +1,23 @@
 from utiles import *
 
-def validacion_intento_ingresado(palabra_ingresada, palabras_ingresadas):
+def validacion_intento_ingresado(palabra_ingresada, lista_palabras_ingresadas):
     '''
     Función: validar_intento_ingresado
     Parámetro:
         palabra_ingresada: cadena de caracteres ingresado por el usuario.
+        lista_palabras_ingresadas: lista de cadenas de caracteres.
     Salidas:
         Devuelve la palabra en mayúscula.
     '''
     palabra_ingresada = cambiar_tilde(palabra_ingresada.lower())
-    while not palabra_ingresada.isalpha() or palabra_ingresada.lower() not in obtener_palabras_validas() or (palabra_ingresada.upper() in palabras_ingresadas):
+    while not palabra_ingresada.isalpha() or palabra_ingresada.lower() not in obtener_palabras_validas() or (palabra_ingresada.upper() in lista_palabras_ingresadas):
         if not palabra_ingresada.isalpha() and len(palabra_ingresada) != 5:
             print("Palabra inválida, tiene que ser de 5 letras y no puede contener número/s ni caracteres especiales.")
         elif len(palabra_ingresada) != 5:
             print("Palabra inválida, tiene que ser de 5 letras.")
         elif palabra_ingresada not in obtener_palabras_validas() and palabra_ingresada.isalpha():
             print("Palabra inválida.")
-        elif palabra_ingresada.upper() in palabras_ingresadas:
+        elif palabra_ingresada.upper() in lista_palabras_ingresadas:
             print("La palabra ya habia sido ingresada.")
         else:#if not palabra_ingresada.isalpha():
             print("Palabra inválida, no puede contener números ni caracteres especiales.")

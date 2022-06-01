cd043dade1092cb0629b4fd8c7103f0dacd3b2ea
Sofia Soler <79118348+sofisoler@users.noreply.github.com>
2022-05-31 23:55:03 -0300

    Update validacion.py

--- TP_Parte_1/fiuble_con_paquetes/validacion.py
+++ TP_Parte_1/fiuble_con_paquetes/validacion.py
@@ -1,38 +1,8 @@
 from utiles import *
 
-def validacion_intento_ingresado(palabra_ingresada, lista_palabras_ingresadas):
-    '''
-    Función: validar_intento_ingresado
-    Descripción:   
-
-    Parámetro:
-        palabra_ingresada: cadena de caracteres ingresado por el usuario.
-        lista_palabras_ingresadas: lista de cadenas de caracteres.
-    Salidas:
-        Devuelve la palabra en mayúscula.
-    '''
-    palabra_ingresada = cambiar_tilde(palabra_ingresada.lower())
-    while not palabra_ingresada.isalpha() or palabra_ingresada.lower() not in obtener_palabras_validas() or (palabra_ingresada.upper() in lista_palabras_ingresadas):
-        if not palabra_ingresada.isalpha() and len(palabra_ingresada) != 5:
-            print('Palabra inválida, tiene que ser de 5 letras y no puede contener número/s ni caracteres especiales.')
-        elif len(palabra_ingresada) != 5:
-            print('Palabra inválida, tiene que ser de 5 letras.')
-        elif palabra_ingresada not in obtener_palabras_validas() and palabra_ingresada.isalpha():
-            print('Palabra inválida.')
-        elif palabra_ingresada.upper() in lista_palabras_ingresadas:
-            print('La palabra ya habia sido ingresada.')
-        else:#if not palabra_ingresada.isalpha():
-            print('Palabra inválida, no puede contener números ni caracteres especiales.')
-        palabra_ingresada = input('Ingrese una palabra valida de 5 letras: ')
-        palabra_ingresada = cambiar_tilde(palabra_ingresada.lower())
-    
-    return palabra_ingresada.upper()
-
 def cambiar_tilde(palabra_ingresada):
     '''
     Función: cambiar_tilde
-    Descripción: 
-
     Parámetro:
         palabra_ingresada: cadena de caracteres ingresado por el usuario.
     Salidas:
@@ -42,5 +12,51 @@ def cambiar_tilde(palabra_ingresada):
     b = 'aeiouyaeiouyaeiouaeiou'
     palabra_sin_acento = palabra_ingresada.maketrans(a, b)
     arriesgo = palabra_ingresada.translate(palabra_sin_acento)
-    
+
     return arriesgo
+
+def no_es_alfabetico (palabra_ingresada):
+    return not palabra_ingresada.isalpha()
+
+def longitud_palabra (palabra_ingresada):
+    return len(palabra_ingresada)!= 5
+
+def validar_palabra (palabra_ingresada):
+    return not no_es_alfabetico (palabra_ingresada) and palabra_ingresada not in obtener_palabras_validas()
+
+def validar_ingreso (palabra_ingresada, intentos_ingresados_str):
+    return palabra_ingresada.upper() in intentos_ingresados_str
+
+def longitud_y_alfabetica(palabra_ingresada):
+    return not palabra_ingresada.isalpha() and len(palabra_ingresada) != 5
+def validacion_intento_ingresado(palabra_ingresada, lista_palabras_ingresadas):
+    '''
+    Función: validar_intento_ingresado
+    Parámetro:
+        palabra_ingresada: cadena de caracteres ingresado por el usuario.
+        lista_palabras_ingresadas: lista de cadenas de caracteres.
+    Salidas:
+        Devuelve la palabra en mayúscula.
+    '''
+    palabra_ingresada = cambiar_tilde(palabra_ingresada.lower())
+    while no_es_alfabetico (palabra_ingresada) or validar_palabra (palabra_ingresada) or validar_ingreso (palabra_ingresada, lista_palabras_ingresadas):
+
+        if longitud_y_alfabetica(palabra_ingresada):
+            print("Palabra inválida, tiene que ser de 5 letras y no puede contener número/s ni caracteres especiales.")
+
+        elif no_es_alfabetico (palabra_ingresada):
+            print("La palabra no tiene que contener numero ni caracteres especiales.")
+
+        elif longitud_palabra (palabra_ingresada): 
+            print("La palabra tiene que ser de 5 letras.")
+
+        elif validar_ingreso (palabra_ingresada, lista_palabras_ingresadas):
+            print("La palabra ya habia sido ingresada.")
+
+        elif validar_palabra (palabra_ingresada):
+            print("La palabra no se encuentra en la lista de palabras válidas.")
+
+        palabra_ingresada = input("Ingrese una palabra valida de 5 letras: ")
+        palabra_ingresada = cambiar_tilde(palabra_ingresada.lower())
+
+    return palabra_ingresada.upper()

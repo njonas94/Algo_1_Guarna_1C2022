d5cc8ade25b7b1877a99fbec9f50041a134b43e3
Aldair-T <86437315+Aldair-T@users.noreply.github.com>
2022-07-03 01:59:39 -0300

    Funcional leer texto y ordenar csv
    Todas las funciones andan al 100

--- TP_Parte_1/fiuble_con_paquetes/leer_archivos.py
+++ TP_Parte_1/fiuble_con_paquetes/leer_archivos.py
@@ -4,7 +4,17 @@ import pandas as pd
 import csv
 
 
-def leer_archivos(archivo):
+def leer_archivos_texto(archivo):
+    """
+    Funcion: leer_archivo_texto
+    Descripcion:
+        Va leyendo el texto, elimina los signos que se encuentren en la palabra (¿?¡!-_.:,;) y los
+        elimina si no son alpha
+    Parametro:
+        archivo: Texto a leer
+    Salida:
+        No se como quiere recibir la persona que necesita esta funcion
+    """
     for linea in archivo:
         if linea:
             linea = re.sub(r'[^\w\s]', '', linea)
@@ -13,15 +23,30 @@ def leer_archivos(archivo):
                 if not palabra.isalpha():
                     linea.remove(palabra)
 
-                    
+
 def ordenar_archivo_partidas(nombre_archivo):
+    """
+    Funcion: ordenar:archivo_partidas
+    Descripcion:
+        Lee el archivo, lo ordena de forma descendente y despues lo pasa al archivo csv
+    Parametros:
+        nombre_archivo: nombre del archivo que se va a ordenar
+    """
     lista_encabezado = ['Fecha partida', 'Hora finalizacion', 'Jugador', 'Aciertos', 'Intentos']
     archivo = pd.read_csv(nombre_archivo)
     archivo_ordenado = archivo.sort_values(by = ['Aciertos'], ascending = [False])
-    archivo_ordenado.to_csv(nombre_archivo, header = lista_encabezado)
+    archivo_ordenado.to_csv(nombre_archivo, header = lista_encabezado, index = False)
 
 
 def crear_archivo_partidas(nombre_archivo, dicc_datos):
+    """
+    Funcion: crear_archivo_partidas
+    Descripcion:
+        Crea el archivo con encabezado y escribe las primeras lineas
+    Parametros:
+        nombre_archivo: nombre para el archivo a crear
+        dicc_datos: diccionario con los datos extraidos de la partida
+    """
     lista_encabezado = ['Fecha partida', 'Hora finalizacion', 'Jugador', 'Aciertos', 'Intentos']
     with open(nombre_archivo, 'w', newline='') as archivo_partidas:
         archivo = csv.DictWriter(archivo_partidas, fieldnames = lista_encabezado)
@@ -30,6 +55,14 @@ def crear_archivo_partidas(nombre_archivo, dicc_datos):
 
 
 def escribir_archivo_partidas(nombre_archivo, dicc_datos):
+    """
+    Funcion: escribir_archivo_partidas
+    Descripcion:
+        Añade una linea con los datos de la partida
+    Parametros:
+        nombre_archivo: nombre del archivo a escribir
+        dicc_datos: diccionario con los datos extraidos de la partida
+    """
     # Si tengo q pasar de dos jugadores, tengo que usar el writerows[dicc1, dicc2]
     lista_encabezado = ['Fecha partida', 'Hora finalizacion', 'Jugador', 'Aciertos', 'Intentos']
     with open(nombre_archivo, 'a', newline='') as archivo_partidas:
@@ -38,6 +71,13 @@ def escribir_archivo_partidas(nombre_archivo, dicc_datos):
 
 
 def registro_partidas(dicc_datos):
+    """
+    Funcion: registro_partidas
+    Descripcion:
+        Determina si el archivo partidas.csv existe, para escribirlo o crearlo
+    Parametros:
+        dicc_datos: diccionario con los datos extraidos de la partida
+    """
     nombre_archivo = 'partidas.csv'
     if not os.path.isfile(nombre_archivo):
         crear_archivo_partidas(nombre_archivo, dicc_datos)
@@ -45,14 +85,17 @@ def registro_partidas(dicc_datos):
         escribir_archivo_partidas(nombre_archivo, dicc_datos)
 
 
-# leer_archivos(archivo = open('cuentos.txt', 'r'))
-dicc1 = {'Fecha partida': '231232', 'Hora finalizacion': '123412', 'Jugador': 'alda', 'Aciertos': 23, 'Intentos': 2}
-dicc2 = {'Fecha partida': '122211', 'Hora finalizacion': '112435', 'Jugador': 'eze', 'Aciertos': 5, 'Intentos': 8}
-dicc3 = {'Fecha partida': '112341', 'Hora finalizacion': '032312', 'Jugador': 'feli', 'Aciertos': 10, 'Intentos': 5}
-dicc4 = {'Fecha partida': '045635', 'Hora finalizacion': '231211', 'Jugador': 'jorge', 'Aciertos': 1, 'Intentos': 3}
+""" 
+Pruebas de las funciones para ver si andan bien
+leer_archivos_texto(archivo = open('cuentos.txt', 'r'))
+dicc1 = {'Fecha partida': '231232', 'Hora finalizacion': '123412', 'Jugador': 'alda', 'Aciertos': 20, 'Intentos': 2}
+dicc2 = {'Fecha partida': '122211', 'Hora finalizacion': '112435', 'Jugador': 'eze', 'Aciertos': 31, 'Intentos': 8}
+dicc3 = {'Fecha partida': '112341', 'Hora finalizacion': '032312', 'Jugador': 'feli', 'Aciertos': 4, 'Intentos': 5}
+dicc4 = {'Fecha partida': '045635', 'Hora finalizacion': '231211', 'Jugador': 'jorge', 'Aciertos': 13, 'Intentos': 3}
 
 registro_partidas(dicc1)
 registro_partidas(dicc2)
 registro_partidas(dicc3)
 registro_partidas(dicc4)
 ordenar_archivo_partidas('partidas.csv')
+"""

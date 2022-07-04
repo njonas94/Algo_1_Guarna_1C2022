ba42a2c9dcd6906eaa81fb54e3ba2609ef8cdb55
Luis <luisangeltrebejo@gmail.com>
2022-07-04 20:38:27 -0300

    Create configuracion.py

---
+++ TP_Parte_1/fiuble_con_paquetes/configuracion.py
@@ -0,0 +1,26 @@
+from interfaz_usuario import *
+
+def impresion(variable,valor,datos,info):
+    default=['7','5','False']
+    if variable!='' and valor=='':
+        print('{}: {} y fue asignada por omisión'.format(variable, valor))
+        datos.append(default[info])
+    elif valor!='' and variable!='':
+        print('{}: {} y fue asignada por configuración'.format(variable, valor))
+        datos.append(valor)
+    return datos
+
+def leer_configuracion ():
+    datos=[]
+    lista_final=[]
+    info=0
+    archivo = open("configuracion.csv", "r")
+    variable, valor= leer_archivo(archivo,',')
+    while (variable!='' and valor=='') or (variable!='' and valor!='' ):
+        lista=impresion(variable,valor,datos,info)
+        variable, valor= leer_archivo(archivo,',')
+        if lista[info].isdigit():
+            lista_final.append(int(lista[info]))
+        else:
+            lista_final.append(bool(lista[info]))
+        info+=1

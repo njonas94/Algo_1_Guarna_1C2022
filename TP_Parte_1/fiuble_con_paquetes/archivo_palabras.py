00f98fca91863651001bfe463deb4b38fe88d943
Luis <luisangeltrebejo@gmail.com>
2022-07-04 01:35:44 -0300

    Update archivo_palabras.py

--- TP_Parte_1/fiuble_con_paquetes/archivo_palabras.py
+++ TP_Parte_1/fiuble_con_paquetes/archivo_palabras.py
@@ -3,3 +3,7 @@ def archivo_palabras(lista_ordenada):
     for palabra in lista_ordenada:
         archivo.write(palabra[0] + "," + str(palabra[1]) + "," + str(palabra[2]) + "," + str(palabra[3]) + "\n")
     archivo.close()
+
+def ordenar_diccionario(diccionario_palabras):
+    lista = sorted(diccionario_palabras.items(), key=lambda x: x[0])
+    return lista
